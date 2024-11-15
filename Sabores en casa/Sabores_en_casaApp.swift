0d7cee956c5f4cd11ed30886aff79c1a08a8cd38
oriana09 <naircostancio@gmailcom>
2024-11-15 15:20:02 -0300

    Initial Commit

---
+++ Sabores en casa/Sabores_en_casaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sabores_en_casaApp.swift
+//  Sabores en casa
+//
+//  Created by Oriana Costancio on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sabores_en_casaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
